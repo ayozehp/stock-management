e4044107df5321172838245171e0678d36f0e8ef
ayoze <ayozehp@hotmail.com>
2024-08-29 19:07:16 +0100

    feat: add database

---
+++ infrastructure/database/main.tf
@@ -0,0 +1,44 @@
+resource "aws_db_instance" "app_db" {
+  allocated_storage      = 20
+  storage_type           = "gp3"
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t4g.micro"
+  db_name                = var.name
+  username               = "admin"
+  password               = "yourpassword" # Replace with a secure password
+  parameter_group_name   = "default.mysql8.0"
+  skip_final_snapshot    = true
+  publicly_accessible    = false
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+}
+
+resource "aws_db_subnet_group" "main" {
+  name       = "main"
+  subnet_ids = data.aws_subnets.subnets.ids
+
+  tags = {
+    Name = "Main subnet group"
+  }
+}
+
+resource "aws_security_group" "db_sg" {
+  name        = "db_sg"
+  description = "Allow MySQL traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = []
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
