6bcd1e9c16aa1725dd68dbfa320ca31f6ddef38c
ayoze <ayozehp@hotmail.com>
2024-09-07 13:07:43 +0100

    feat: add terraform code to configure ECS

---
+++ infrastructure/container/main.tf
@@ -0,0 +1,139 @@
+resource "aws_ecs_cluster" "app_cluster" {
+  name = "${var.name}-cluster"
+}
+
+resource "aws_ecs_task_definition" "app_task" {
+  family                   = var.name
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name  = var.name
+      image = "${var.repository}:${var.tag}"
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+        }
+      ],
+      healthCheck = {
+        command     = ["CMD-SHELL", "curl -f http://localhost:8080/car_stock/home.action || exit 1"]
+        interval    = 30
+        timeout     = 5
+        retries     = 3
+        startPeriod = 10
+      }
+    }
+  ])
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.name}EcsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_service" "app_service" {
+  name            = "${var.name}-service"
+  cluster         = aws_ecs_cluster.app_cluster.id
+  task_definition = aws_ecs_task_definition.app_task.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets          = data.aws_subnets.subnets.ids
+    assign_public_ip = true
+    security_groups  = [aws_security_group.service_security_group.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn
+    container_name   = aws_ecs_task_definition.app_task.family
+    container_port   = 8080
+  }
+}
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.load_balancer_security_group.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_alb" "application_load_balancer" {
+  name               = "${var.name}-load-balancer"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.subnets.ids
+  security_groups    = [aws_security_group.load_balancer_security_group.id]
+}
+
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 80
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow traffic in from all sources
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "${var.name}-target-group"
+  port        = 8080
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = data.aws_vpc.default.id
+
+  health_check {
+    path = "/car_stock/home.action"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_alb.application_load_balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
---
+++ infrastructure/container/variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  description = "Name of the app"
+}
+
+variable "repository" {
+  description = "Images repository use to deploy"
+}
+
+variable "tag" {
+  description = "Tag of the image to deploy"
+}
