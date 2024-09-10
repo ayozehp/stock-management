c12712e5c3e49b7c6dfc51bb4bcfec44de821d73
ayoze <ayozehp@hotmail.com>
2024-09-10 21:16:03 +0100

    refactor: change order of resources in the main file

--- infrastructure/container/main.tf
+++ infrastructure/container/main.tf
@@ -96,6 +96,16 @@ resource "aws_alb" "application_load_balancer" {
   security_groups    = [aws_security_group.load_balancer_security_group.id]
 }
 
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_alb.application_load_balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
+
 resource "aws_security_group" "load_balancer_security_group" {
   ingress {
     from_port   = 80
@@ -128,12 +138,3 @@ resource "aws_lb_target_group" "target_group" {
   }
 }
 
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_alb.application_load_balancer.arn
-  port              = 80
-  protocol          = "HTTP"
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.target_group.arn
-  }
-}
